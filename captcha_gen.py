0f17c8e6966edba42a881f63eb5c596b6a7892ac
Junoh Moon <snowphone@XPS-13.localdomain>
2018-05-27 23:40:13 +0900

    refactor scg function,  separate write function from scg, rename scg

--- captcha_gen.py
+++ captcha_gen.py
@@ -5,36 +5,45 @@ from claptcha import Claptcha
 import os
 
 
-word_dic = {}
 
-def scg(size, noise=0.5, length =1, font="C:/Windows/Fonts/ARLRDBD.TTF"):
+def generate_captcha(noise=0.5, length =1):
+	"""
+	It returns captcha object.
+	"""
+	def isfont(filename):
+		return True if filename.find(".ttf") != -1 or filename.find(".otf") != -1 else False
 
-    # def randomString():
-    #     rndLetters = (random.choice(string.ascii_uppercase) for _ in range(6))
-    #     return "".join(rndLetters)
+	def randomChar(length):
+		return "".join(random.choices(string.digits, k=length))
+	fonts = [font for font in os.listdir("./fonts/") if isfont(font)]
+	
 
-    def randomChar():
-        rndChars = (random.choice(string.ascii_uppercase) for _ in range(length))
-        return "".join(rndChars)
+	# def randomString():
+	#     rndLetters = (random.choice(string.ascii_uppercase) for _ in range(6))
+	#     return "".join(rndLetters)
 
-    # Initialize Claptcha object with random text, FreeMono as font, of size
-    # 100x30px, using bicubic resampling filter and adding a bit of white noise
-    #print(randomChar)
-    c = Claptcha(randomChar(), font, (size,size), resample=Image.BILINEAR, noise=0.55)
-    filename = c.source
-    
-    #font var, 1char pront? , font,
-    #noise, font, 
-    #size, margin out
-    if filename in word_dic.keys() :
-        word_dic[filename] = word_dic[filename] + 1
-    else : 
-        word_dic[filename] = 1
-    text, _ = c.write(filename +'(' + str(word_dic[filename]) +  ').png')
-    print(text)  # string printed into filename.png
+	font = "./fonts/" + random.choice(fonts)
+	size = random.randint(100, 200)
+
+	# Initialize Claptcha object with random text, FreeMono as font, of size
+	# 100x30px, using bicubic resampling filter and adding a bit of white noise
+	captcha = Claptcha(randomChar(length), font, (size * length, size), resample=Image.BILINEAR, noise=noise)
+	return captcha
+
+def write_captcha(captcha : Claptcha, init_count_val=None):
+	if init_count_val:
+		#write_captcha.counter: static variable
+		write_captcha.counter = init_count_val
+	else:
+		write_captcha.counter += 1
+
+	captcha.write("{cnt:05d}_label_{label:s}.png".format(cnt=write_captcha.counter, label=captcha.source))
+	return
+write_captcha.counter = 0
+
+
+
 
 if __name__ == "__main__":
-    i=0
-    while i < 10:
-        i = i + 1
-        scg(450, 0.3)
+	for _ in range(10):
+		generate_captcha(noise=0.3)
