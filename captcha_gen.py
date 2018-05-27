d25c20ee0d4565d0e1526e36e0b5ef9f973d851e
Junoh Moon <mjo970625@gmail.com>
2018-05-28 00:37:59 +0900

    add 'white background', 'ground-truth-box' function. v1 completed

--- captcha_gen.py
+++ captcha_gen.py
@@ -1,12 +1,14 @@
+import math
+import os
 import random
 import string
-from PIL import Image
+
+import PIL
 from claptcha import Claptcha
-import os
+from PIL import Image
 
 
-
-def generate_captcha(noise=0.5, length =1):
+def generate_captcha(noise=0.5, length=1):
 	"""
 	It returns captcha object.
 	"""
@@ -16,7 +18,6 @@ def generate_captcha(noise=0.5, length =1):
 	def randomChar(length):
 		return "".join(random.choices(string.digits, k=length))
 	fonts = [font for font in os.listdir("./fonts/") if isfont(font)]
-	
 
 	# def randomString():
 	#     rndLetters = (random.choice(string.ascii_uppercase) for _ in range(6))
@@ -27,23 +28,59 @@ def generate_captcha(noise=0.5, length =1):
 
 	# Initialize Claptcha object with random text, FreeMono as font, of size
 	# 100x30px, using bicubic resampling filter and adding a bit of white noise
-	captcha = Claptcha(randomChar(length), font, (size * length, size), resample=Image.BILINEAR, noise=noise)
+	captcha = Claptcha(randomChar(length), font, (size * length, size),
+	                   margin=(1, 1), resample=Image.BILINEAR, noise=noise)
 	return captcha
 
-def write_captcha(captcha : Claptcha, init_count_val=None):
-	if init_count_val:
-		#write_captcha.counter: static variable
-		write_captcha.counter = init_count_val
-	else:
-		write_captcha.counter += 1
 
-	captcha.write("{cnt:05d}_label_{label:s}.png".format(cnt=write_captcha.counter, label=captcha.source))
+def add_background(captcha: Claptcha):
+	'''
+	return: (roi, image)
+	'''
+	def get_random_ROI():
+		leftupper = (random.randint(0, captcha.w), random.randint(0, captcha.h))
+		rightlower = (leftupper[0] + captcha.w, leftupper[1] + captcha.h)
+		return (*leftupper, *rightlower)
+	# white background
+	background = Image.new(
+		"RGB", (captcha.w * 2, captcha.h * 2), color=(255, 255, 255))
+	roi = get_random_ROI()
+	background.paste(captcha.image[1], roi)
+	return (roi, background)
+
+
+def write_captcha(image: Image.Image, filename_without_extension):
+	folder = "./img/"
+	os.makedirs(folder, exist_ok=True)
+	return image.save(folder + filename_without_extension + ".jpg")
+
+
+def make_ground_truth_box(roi, image: Image.Image, file_basename):
+	'''
+	roi: (leftupper.x, leftupper.y, rightlower.x, rightlower.y)
+	'''
+	folder = "./img/"
+	os.makedirs(folder, exist_ok=True)
+
+	roi_width = roi[2] - roi[0]
+	roi_height = roi[3] - roi[1]
+	center = ((roi[0] + roi[2]) / 2, (roi[1] + roi[3]) / 2)
+
+	with open(folder + file_basename + ".txt", mode="w+") as f:
+		f.write("{index:d} {x:f} {y:f} {width:f} {height:f}\n".format(
+			index=0, x=center[0] / image.width,
+			y=center[1] / image.height,
+			width=roi_width / image.width,
+			height=roi_height/image.height))
 	return
-write_captcha.counter = 0
-
-
 
 
 if __name__ == "__main__":
-	for _ in range(10):
-		generate_captcha(noise=0.3)
+	for i in range(10):
+		captcha = generate_captcha(noise=0.3)
+		file_basename = "{cnt:05d}_label_{label:s}".format(
+			cnt=i, label=captcha.source)
+		roi, image = add_background(captcha)
+		make_ground_truth_box(roi, image, file_basename)
+		write_captcha(image, file_basename)
+		print("{}th image, label: {}".format(i, captcha.text))
