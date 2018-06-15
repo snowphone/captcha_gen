62e384c5ca6f5c80d4cb156e968a141ba314c6f5
Junoh Moon <JOMoon@material.None>
2018-06-15 15:54:37 +0900

    클래스로 리팩토링, 학습에 문자열 숫자 사용

--- captcha_gen.py
+++ captcha_gen.py
@@ -20,85 +20,121 @@ from claptcha import Claptcha
 from PIL import Image
 
 
-def generate_captcha(noise=0.5, length=1):
-	"""
-	It returns captcha object.
-	"""
-	def isfont(filename):
-		return True if filename.find(".ttf") != -1 or filename.find(".otf") != -1 else False
+class Captcha_image():
 
-	def randomChar(length):
-		return "".join(random.choices(string.digits, k=length))
-	fonts = [font for font in os.listdir("./fonts/") if isfont(font)]
+	def __init__(self, noise=0, valid=False):
+		self.captcha_list = []
+		self.roi_list = []
+		self.noise = noise
+		self.valid = valid
+		self.max_size = 200
+		self.max_captcha_num = 5
+		self.image = Image.new( 
+				"RGB", 
+				(int(self.max_size * 0.9 * (self.max_captcha_num + 1)), self.max_size * 2),			
+				color=(255, 255, 255))
 
+		self._generate_captcha()
+		self._add_background()
+		return
+	
+	def _generate_captcha(self):
+		"""
+		It returns captcha object.
+		"""
+		def isfont(filename):
+			return True if filename.find(".ttf") != -1 or filename.find(".otf") != -1 else False
 
-	font = "./fonts/" + random.choice(fonts)
-	size = random.randint(100, 200)
-
-	captcha = Claptcha(randomChar(length), font, (size * length, size),
-					   margin=(1, 1), resample=Image.BILINEAR, noise=noise)
-	return captcha
-
-
-def add_background(captcha: Claptcha):
-	'''
-	return: (roi, image)
-	'''
-	def get_random_ROI():
-		leftupper = (random.randint(0, captcha.w), random.randint(0, captcha.h))
-		rightlower = (leftupper[0] + captcha.w, leftupper[1] + captcha.h)
-		return (*leftupper, *rightlower)
-	# white background
-	background = Image.new(
-		"RGB", (captcha.w * 2, captcha.h * 2), color=(255, 255, 255))
-	roi = get_random_ROI()
-	background.paste(captcha.image[1], roi)
-	return (roi, background)
+		def randomChar():
+			return random.choice(string.digits)
+		fonts = [font for font in os.listdir("./fonts/") if isfont(font)]
 
 
-def write_captcha(image: Image.Image, filename_without_extension):
-	folder = "./img/"
-	os.makedirs(folder, exist_ok=True)
-	return image.save(folder + filename_without_extension + ".jpg")
+		font = "./fonts/" + random.choice(fonts)
+		size = random.randint(self.max_size // 2, self.max_size)
+
+		self.captcha_list = [ Claptcha(
+			randomChar(), 
+			font, 
+			(size * 0.9 , size),
+			margin=(0, 0), 
+			resample=Image.BILINEAR, 
+			noise=self.noise) 
+			for _ in range(random.randint(1, self.max_captcha_num))]
+
+		return self.captcha_list
+	
+	def _add_background(self):
+
+		captcha_num = len(self.captcha_list)
+		width = self.captcha_list[0].w 
+		height = self.captcha_list[0].h
+
+		def get_random_roi(width, height):
+			leftupper = (random.randint(0, width), random.randint(0, height))
+			rightlower = (leftupper[0] + width, leftupper[1] + height)
+			return (leftupper, rightlower)
+
+		def split_roi(leftupper, rightlower):
+			ret = []
+			for index in range(captcha_num):
+				x = leftupper[0] + index * width
+				y = leftupper[1]
+				roi = (x, y, x + width, y + height)
+				ret.append(roi)
+			return ret
+
+		_roi = get_random_roi(width * captcha_num, height)
+		self.roi_list = split_roi(*_roi)
+
+		for roi, captcha in zip(self.roi_list, self.captcha_list):
+			self.image.paste(captcha.image[1], roi)
+		return 
 
 
-def make_ground_truth_box(class_index, roi, image: Image.Image, file_basename):
-	'''
-	roi: (leftupper.x, leftupper.y, rightlower.x, rightlower.y)
-	'''
-	folder = "./img/"
-	os.makedirs(folder, exist_ok=True)
+	def save(self, filename_without_extension):
+		folder = "./img/"
+		os.makedirs(folder, exist_ok=True)
 
-	roi_width = roi[2] - roi[0]
-	roi_height = roi[3] - roi[1]
-	center = ((roi[0] + roi[2]) / 2, (roi[1] + roi[3]) / 2)
+		if not self.valid:
+			self._make_ground_truth_box(filename_without_extension)
 
-	with open(folder + file_basename + ".txt", mode="w+") as f:
-		f.write("{class_index} {x:f} {y:f} {width:f} {height:f}\n".format(
-			class_index=class_index, x=center[0] / image.width,
-			y=center[1] / image.height,
-			width=roi_width / image.width,
-			height=roi_height/image.height))
-	return
+		return self.image.save(folder + filename_without_extension + ".jpg")
+
+
+	def _make_ground_truth_box(self, file_basename):
+		'''
+		roi: (leftupper.x, leftupper.y, rightlower.x, rightlower.y)
+		'''
+		folder = "./img/"
+
+		with open(folder + file_basename + ".txt", mode="w+") as f:
+			for roi, captcha in zip(self.roi_list, self.captcha_list):
+				roi_width = roi[2] - roi[0]
+				roi_height = roi[3] - roi[1]
+				center = ((roi[0] + roi[2]) / 2, (roi[1] + roi[3]) / 2)
+
+				f.write("{class_index} {x:f} {y:f} {width:f} {height:f}\n".format(
+					class_index=captcha.text,
+					x=center[0] / self.image.width,
+					y=center[1] / self.image.height,
+					width=roi_width / self.image.width,
+					height=roi_height / self.image.height)
+					)
+		return
+	#end of Captcha_image 
 
 
 if __name__ == "__main__":
-	if len(argv) < 2:
+	try:
+		num_of_captcha = int(argv[1])
+	except IndexError as err:
 		print("Usage: {} <numbers of captcha> [-valid]".format(argv[0]), file=sys.stderr)
 		exit(1)
 
-	num_of_captcha = int(argv[1])
-
-
 	for i in range(num_of_captcha):
-		if argv.count("-valid"):
-			length = random.randint(2, 6)
-		else:
-			length = 1
-		captcha = generate_captcha(noise=0, length=length)
-		file_basename = "{cnt:05d}_label_{label:s}".format(
-			cnt=i, label=captcha.source)
-		roi, image = add_background(captcha)
-		make_ground_truth_box(captcha.source, roi, image, file_basename)
-		write_captcha(image, file_basename)
-		print("{}th image, label: {}".format(i, captcha.text))
+		captcha = Captcha_image()
+		label = "".join([cap.text for cap in captcha.captcha_list])
+		file_basename = "{cnt:05d}_label_{label:s}".format( cnt=i, label=label )
+		captcha.save(file_basename)
+		print("{}th image, label: {}".format(i, label))
